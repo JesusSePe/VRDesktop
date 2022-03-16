310fe42665a88f4ee251bce51f29766506c8110c
danii0904 <danielnupa0904@gmail.com>
2022-03-16 14:15:12 +0100

    Desktop with courses listed

---
+++ src/main/java/application/createCursosController.java
@@ -0,0 +1,29 @@
+package application;
+import java.util.ResourceBundle;
+
+import javax.print.DocFlavor.URL;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class createCursosController {
+	
+		@FXML
+	    private TextArea taDescripcio;
+	
+	    @FXML
+	    private TextField inputTitol;
+
+	    @FXML
+	    void initialize() {
+
+	    }
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
